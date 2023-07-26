3191bdebd4fa6d0493b87fb7716f9c51bd0dd2ea
AbdullahDsouky220 <abdallahdsoukyabdelnabyhassen30_sd@science.bsu.edu.eg>
2023-07-26 16:13:41 +0300

    first commit

---
+++ app/api/auth/[...nextauth]/route.jsx
@@ -0,0 +1,75 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from '@/lib/prismadb'
+import CredentialsProvider from "next-auth/providers/credentials"
+import GoogleProvider from "next-auth/providers/google"
+import GithubProvider from 'next-auth/providers/github'
+
+
+export const authOptions={
+    adapter:PrismaAdapter(prisma),
+    providers:[
+        GoogleProvider({
+            clientId:process.env.GOOGLE_ID,
+            clientSecrent:process.env.GOOGLE_ID,
+        }),
+        GithubProvider({
+            clientId:process.env.Github_ID,
+            clientSecrent:process.env.Github_ID,
+        }),
+        CredentialsProvider({
+            name:"credentials",
+            credentials:{
+                
+                email:{
+                    label:"Email",
+                    type:"text",
+                    placeholder:"Abdullah"
+                },
+                password:{
+                    label:"Password",
+                    type:"password",
+                },
+                username:{
+                    label:"Username",
+                    type:"text",
+                    placeholder:"Abdullah Dsouky"
+                },
+            },
+            async authorize(credentials){
+                const user={
+                    id:1,
+                    name:'abdullah dsou',
+                    email:"abdodsouky@gmail.com"
+                }
+                return user
+            }
+        })
+    ],
+    secret:process.env.SECRET,
+    debug: process.env.NODE_ENV === "development",
+    session:{
+        strategy:"jwt"
+    }
+
+}
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
+
+
+//adapter
+//providers 
+   //googleprodider
+        //clientid and client secret
+   //githubprovder
+        //clientid and client secret
+   //credientials
+        //name and credientals you neeed like user name and password 
+                //authorize(credienals)
+//secet from env file must be unique
+// debug: process.env.NODE_ENV === "development",
+//session:{
+//     strategy:"jwt"
+// }
